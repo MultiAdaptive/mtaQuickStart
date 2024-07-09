10225c56f7f06a8c54b2a3199bb3e42f9c4e8d47
WuEcho <emailforecho@163.com>
2024-07-09 18:20:19 +0800

    add script

---
+++ bin/init.sh
@@ -0,0 +1,348 @@
+#!/bin/bash
+
+# init env or getting start
+ACTION="init env"
+
+# join or create chain
+TYPE=""
+
+# bin path
+MULTIADAPTIVE_BIN=$(pwd -P)
+
+# the bash home path
+MULTIADAPTIVE_HOME_PATH=${MULTIADAPTIVE_BIN%/bin}
+
+
+#root path
+ROOT_PATH="/home/ubuntu/"
+
+CHAIN_INFO_FILE=""
+CHAIN_CONF_DIR=""
+CHAIN_DATA_DIR=""
+
+# MultiAdaptive broadcast node ,MultiAdaptive storage node
+# value  "b"  match the node type is MultiAdaptive broadcast node
+# value  "s" match the node type is MultiAdaptive storage node
+NODETYPE="b"
+
+
+# MultiAdaptive suport two kind of chain type ：one of them is eth,another is btc
+CHAINTYPE="eth"
+
+# go
+DOWNLOAD_COMMOND="curl -sSL https://golang.org/dl/go1.21.6.linux-amd64.tar.gz | sudo tar -C /usr/local -xz"
+
+#eth scan url
+L1Url=""
+
+#btc scan host
+BTCHost=""
+
+BTCUser=""
+
+BTCPWD=""
+
+BTCPriv=""
+
+# chain id
+ChainID=11155111
+
+# boot to p2p connect
+BOOTNODEINFO="enode://dbffc218798fd2febbb1106aa910d336b33bc1b01267a9181b7411af57b37751f9ebcf24e5264dd5fc7fb7572d799d5830882445de76e334590d4662c2a23034@13.212.115.195:30303"
+
+# initialize a utopia blockchain
+initenv_getstart() {
+    PS3="Please pick an option: "
+    select opt in "init env" "getting start"; do
+        case "$REPLY" in
+            1 ) ACTION="init env"; break;;
+            2 ) ACTION="getting start"; break;;
+            *) echo "Invalid option, please retry";;
+        esac
+    done
+    
+    select_node_type
+}
+
+select_node_type() {
+    NODETYPE="b"
+    if [ "$ACTION" == "init env" ];then
+            ##init env
+            echo "exec apt-get updating...."
+            sudo apt-get update
+        if [ $? -eq 0 ]; then
+              echo "normal node"
+              install_go
+              install_git
+              install_nettool
+              install_make
+              install_gcc
+        else
+            echo "Sudo apt-get update failed，please check or do it on your own。"
+        fi
+    
+    else
+        # starting a node
+        echo "do nothing"
+    fi
+}
+
+install_go() {
+    echo "Download go package and uncompressing....."
+    eval "$DOWNLOAD_COMMOND"
+    if [ $? -eq 0 ]; then
+        echo "Download and uncompressed success."
+        # 添加Go相关环境变量到~/.bashrc
+        echo -e "export PATH=\$PATH:/usr/local/go/bin\nexport GOPATH=/home/ubuntu\nexport PATH=\$PATH:/home/ubuntu/bin" >> ~/.bashrc
+        echo "Go env needed is done."
+        
+    else
+        echo "Download and uncompressed failed，please check go or do it on your own：$DOWNLOAD_COMMOND"
+    fi
+}
+
+install_git() {
+    echo "Update git....."
+    sudo apt-get install -y git
+    if [ $? -eq 0 ]; then
+        echo "Git is success."
+    else
+        echo "Git update failed，please check git or do it on your own."
+    fi
+}
+
+install_nettool() {
+    echo "Update net-tool....."
+    sudo apt install net-tools
+    if [ $? -eq 0 ]; then
+        echo "net-tool update successful."
+    else
+        echo "net-tool update failed，please check net-tool or do it on your own."
+    fi
+}
+
+install_make() {
+    echo "Update make...."
+    sudo apt-get install make
+    if [ $? -eq 0 ]; then
+       echo "Update make success"
+    else
+       echo "Update make failed"
+    fi
+}
+
+install_gcc() {
+    echo "Update gcc...."
+    sudo apt-get install gcc
+    if [ $? -eq 0 ]; then
+       echo "Update make success"
+       
+       echo 'NOTE!!! You should do !!!:  1.Do source /home/ubuntu/.bashrc. When initenv.sh is finished.'
+       go env -w CGO_ENABLED=1
+    else
+       echo "Update gcc failed"
+    fi
+
+}
+
+init_datapath() {
+    CHAIN_DATA_DIR=$MULTIADAPTIVE_HOME_PATH/chain/$ChainID
+    CHAIN_CONF_DIR=$MULTIADAPTIVE_HOME_PATH/conf/$ChainID
+    CHAIN_INFO_FILE=$CHAIN_CONF_DIR/chain-info.properties
+    
+    # remove existing one
+    if [ -d "$CHAIN_DATA_DIR" ];then
+        rm -rf $CHAIN_DATA_DIR
+        rm -rf $CHAIN_CONF_DIR
+    fi
+    
+    mkdir -p $CHAIN_DATA_DIR/DataDir
+    mkdir -p $CHAIN_CONF_DIR
+    
+    touch $CHAIN_INFO_FILE
+}
+
+# create account
+new_account() {
+    echo "Create account..."
+    
+    SAVEDSTTY=`stty -g`
+    stty -echo
+    echo "Enter your password:"
+    read PASSWORD
+    stty $SAVEDSTTY
+
+    $MULTIADAPTIVE_BIN/geth account new --datadir "$CHAIN_HOME_DIR/data"
+   
+    touch $CHAIN_HOME_DIR/data/keystore/.password.txt
+
+    echo "$PASSWORD" > $CHAIN_HOME_DIR/data/keystore/.password.txt
+
+    get_account_address
+}
+
+
+# get account address
+get_account_address() {
+
+    ADDRESS_LIST=`$data/geth  account list --datadir "$CHAIN_HOME_DIR/data"`
+    if  [ ! -n "$ADDRESS_LIST" ];then
+        new_account
+    else
+        ADDRESS=`echo $ADDRESS_LIST | cut -d '{' -f 2 | cut -d '}' -f 1`
+    fi
+
+    echo "address=0x$ADDRESS" >> $CHAIN_INFO_FILE
+}
+
+
+config_url() {
+    echo "Please conf the environment variable file...."
+    
+    if [ "$CHAINTYPE" == "eth" ];then
+        echo
+        PS3="Please pick an option that Kind of L1 RPC you're connecting to, used to inform DA data receipts fetching.: "
+        select opt in "alchemy" "quicknode" "infura" "parity" "nethermind" "debug_geth" "erigon" "basic" "any"; do
+        case "$REPLY" in
+            1 ) L1_RPC_KIND="alchemy"; break;;
+            2 ) L1_RPC_KIND="quicknode"; break;;
+            3 ) L1_RPC_KIND="infura"; break;;
+            4 ) L1_RPC_KIND="parity"; break;;
+            5 ) L1_RPC_KIND="nethermind"; break;;
+            6 ) L1_RPC_KIND="debug_geth"; break;;
+            7 ) L1_RPC_KIND="erigon"; break;;
+            8 ) L1_RPC_KIND="any"; break;;
+            *) echo "Invalid option, please retry";;
+        esac
+        done
+        echo
+        
+            while true; do
+            read -p "Enter the L1Url: " L1Url
+            # 判断输入是否为空
+            if [ -z "$L1Url" ]; then
+                echo "Error: Input cannot be empty. Please enter a valid L1Url."
+            else
+                break  # 用户输入不为空，退出循环
+            fi
+            done
+
+            echo "L1 RPC URL will written into $CHAIN_INFO_FILE later."
+            echo
+    else
+            while true; do
+            read -p "Enter the BTC URL you want to scan: " BTCHost
+            # 判断输入是否为空
+            if [ -z "$BTCHost" ]; then
+                echo "Error: Input cannot be empty. Please enter a valid BTC URL."
+            else
+                break  # 用户输入不为空，退出循环
+            fi
+            done
+        
+            echo
+        
+            while true; do
+            read -p "Enter the BTC URL User you want config it can be empty: " BTCUser
+            done
+            
+            echo
+            
+            while true; do
+            read -p "Enter the BTC URL password you want config it can be empty: " BTCPWD
+            done
+            
+            echo
+            
+            while true; do
+            read -p "Enter the BTC Private key :" BTCPriv
+            # 判断输入是否为空
+            if [ -z "$BTCPriv" ]; then
+                echo "Error: Input cannot be empty. Please enter a valid BTC PrivateKey ."
+            else
+                break  # 用户输入不为空，退出循环
+            fi
+            done
+    fi
+   
+}
+
+chose_chain_type() {
+    echo "Please chose a chain type to run...."
+    echo
+    PS3="Please pick an option that type of multiadaptive suport :"
+    select opt in "eth" "btc"; do
+        case "$REPLY" in
+            1 ) CHAINTYPE="eth"; break;;
+            2 ) CHAINTYPE="btc"; break;;
+            *) echo "Invalid option, please retry";;
+        esac
+    done
+    echo
+
+}
+
+
+
+P2P_PORT=30303
+
+write_env_conf() {
+    echo "writting chain config into $CHAIN_INFO_FILE"
+    # write net info
+    echo "ChainID=$ChainID" >> $CHAIN_INFO_FILE
+    echo "host=127.0.0.1" >> $CHAIN_INFO_FILE
+    echo "p2p_port=$P2P_PORT" >> $CHAIN_INFO_FILE
+    echo "NODETYPE=$NODETYPE" >> $CHAIN_INFO_FILE
+    echo "TYPE=$TYPE" >> $CHAIN_INFO_FILE
+    echo "CHAINTYPE=$CHAINTYPE" >> $CHAIN_INFO_FILE
+    # write bootnode info
+    echo "bootnode=$BOOTNODEINFO" >> $CHAIN_INFO_FILE
+    
+    if [ "$CHAINTYPE" == "eth" ];then
+        echo "L1Url=$L1Url" > $CHAIN_INFO_FILE
+    else
+        echo "BTCHost=$BTCHost" > $CHAIN_INFO_FILE
+        echo "BTCUser=$BTCUser" > $CHAIN_INFO_FILE
+        echo "BTCPriv=$BTCPriv" > $CHAIN_INFO_FILE
+    fi
+    echo "chain config info is written into $CHAIN_INFO_FILE,please check that."
+    echo
+}
+
+init_geth() {
+    echo "starting init geth data...."
+    
+    cd "$MULTIADAPTIVE_BIN"
+    
+    cp $MULTIADAPTIVE_HOME_PATH/chain/genesis.json  $CHAIN_DATA_DIR
+    
+    #需要执行domicon geth
+    ./geth init --datadir="$CHAIN_DATA_DIR/data" "$CHAIN_DATA_DIR/genesis.json" && wait
+
+    echo "geth is inited."
+    echo
+    
+}
+
+
+main() {
+    initenv_getstart
+    if [ "$ACTION" == "getting start" ];then
+        ##创建目录
+        init_datapath
+        ##创建账户
+        get_account_address
+      
+        ##选择了链的类型
+        chose_chain_type
+    
+        ##填写l1 url
+        config_url
+        ##将配置记录下来
+        write_env_conf
+        #init geth
+        init_geth
+    fi
+}
+
+main
---
+++ bin/start.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# bin path
+MULTIADAPTIVE_BIN=$(pwd -P)
+
+# the bash home path
+MULTIADAPTIVE_HOME_PATH=${MULTIADAPTIVE_BIN%/bin}
+
+# lib path
+DOMICON_ENV="${DOMICON_HOME_PATH}/env"
+
+DOMICON_PKG="${DOMICON_HOME_PATH}/packages"
+
+#root path
+ROOT_PATH="/home/ubuntu/"
+
+CHAIN_INFO_FILE=""
+CHAIN_CONF_DIR=""
+CHAIN_DATA_DIR=""
+
+# MultiAdaptive broadcast node ,MultiAdaptive storage node
+# value  "b"  match the node type is MultiAdaptive broadcast node
+# value  "s" match the node type is MultiAdaptive storage node
+NODETYPE="b"
+
+ChainID=11155111
+
+# join or create chain
+TYPE=""
+
+p2p_port=
+
+bootnode=
+
+L1Url=
+
+#btc scan host
+BTCHost=""
+
+BTCUser=""
+
+BTCPWD=""
+
+BTCPriv=""
+
+CHAINTYPE=
+
+
+read_chain_conf() {
+    first_folder=$(find $MULTIADAPTIVE_HOME_PATH/conf -mindepth 1 -maxdepth 1 -type d -print -quit)
+    first_folder_name=$(basename "$first_folder")
+    CHAIN_CONF_DIR=$MULTIADAPTIVE_HOME_PATH/conf/$first_folder_name
+    CHAIN_DATA_DIR=$MULTIADAPTIVE_HOME_PATH/chain/$first_folder_name/data
+    echo "read chain config in $CHAIN_CONF_DIR"
+    CHAIN_INFO_FILE=$MULTIADAPTIVE_HOME_PATH/conf/$first_folder_name/chain-info.properties
+    
+    if [ -f "$CHAIN_INFO_FILE" ];then
+        while IFS='=' read -r key value
+        do
+            key=$(echo $key | tr '.' '_')
+            eval ${key}=\${value}
+        done < "$CHAIN_INFO_FILE"
+    else
+        echo "$CHAIN_INFO_FILE not found, existing."
+        exit 0
+    fi
+    
+}
+
+
+start_geth() {
+    echo "getting start with geth...."
+
+    cd $MULTIADAPTIVE_BIN
+    
+    nohup ./geth --datadir $CHAIN_DATA_DIR --http --http.corsdomain=* --http.vhosts=* --http.addr=0.0.0.0 --http.api=eth,net --ws --ws.addr=0.0.0.0 --ws.origins=* --ws.api=eth,net--syncmode=full --gcmode=archive --maxpeers=10 --networkid=$ChainID --authrpc.vhosts=*  --l1Url $L1Url --bootnodes $bootnode >> $CHAIN_DATA_DIR/geth.log 2>&1 &
+    
+    pidFile="$CHAIN_CONF_DIR/geth.pid"
+    if [ ! -f $pidFile ];then
+         touch $pidFile
+    fi
+
+    echo $! > $pidFile
+    echo "geth is started. pid is written into $pidFile."
+    echo
+}
+
+start_btc() {
+    echo "getting start with btc...."
+
+    cd $MULTIADAPTIVE_BIN
+    
+    nohup ./geth --datadir $CHAIN_DATA_DIR --http --http.corsdomain=* --http.vhosts=* --http.addr=0.0.0.0 --http.api=eth,net --ws --ws.addr=0.0.0.0 --ws.origins=* --ws.api=eth,net--syncmode=full --gcmode=archive --maxpeers=10 --l1Host $BTCHost --l1User $BTCUser --l1Password $BTCPWD --nodeType $NODETYPE --btcPrivate $BTCPriv --chainName bitcoin  --networkid=$ChainID --bootnodes $bootnode >> $CHAIN_DATA_DIR/geth.log 2>&1 &
+    
+    pidFile="$CHAIN_CONF_DIR/geth.pid"
+    if [ ! -f $pidFile ];then
+         touch $pidFile
+    fi
+
+    echo $! > $pidFile
+    echo "geth is started. pid is written into $pidFile."
+    echo
+
+
+}
+
+
+# write bootnodes file
+write_bootnodes_file() {
+    echo "find self bootnode info from log."
+    
+    bootnodeFile="$CHAIN_CONF_DIR/bootnode.txt"
+    if ! test -e $bootnodeFile;then
+         touch $bootnodeFile
+    fi
+
+    i=1
+    while true
+    do
+        num_bootnodes=`grep -c 'Started P2P networking' $CHAIN_DATA_DIR/geth.log`
+        if [ $num_bootnodes -ne 0 ];then
+            bootnode=`grep 'Started P2P networking' $CHAIN_DATA_DIR/geth.log`
+            bootnode=`echo $bootnode | cut -d '=' -f 2 | cut -d ' ' -f 2`
+            ips=0
+            for i in `ifconfig | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | grep -v '127\|255\|0.0.0.0'`;do
+            if [ $ips -eq 0 ];then
+              modified=$(echo "$bootnode" | sed "s/@[^:]*:/@$i:/")
+              echo -n "$modified" >> $bootnodeFile;
+            else
+              modified=$(echo "$bootnode" | sed 's/@[^:]*:/@"$i":/')
+              echo -n "$modified" >> $bootnodeFile;
+            fi
+            let ips++
+            done
+            break
+        else
+         i=`expr ${i} + 1`
+         if [ $i -gt 7 ];then
+            echo  "can not find bootnode info, utopia start may have failed, please check $CHAIN_DATA_DIR/geth.log"
+            break
+         else
+            sleep 1
+         fi
+    fi
+    done
+}
+
+
+main() {
+    read_chain_conf
+    
+    if [ "$CHAINTYPE" == "eth" ];then
+        start_geth
+    else
+        start_btc
+    fi
+    write_bootnodes_file
+}
+
+main
